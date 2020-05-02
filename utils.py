a5b2d629e9a8891da9cfe455d38aea17276cfa04
Brian <doucetba@gmail.com>
2020-05-02 09:58:48 -0400

    Updated return statement for get_valid_end_date

--- utils.py
+++ utils.py
@@ -9,11 +9,8 @@ def get_formatted_datetime(date_to_format: datetime) -> str:
     return date_to_format.strftime(datetime_format)
 
 
-def get_valid_end_date(end_date: datetime):
+def get_valid_end_date(end_date: datetime) -> bool:
     yesterday = datetime.today() - timedelta(days=1)
     end_date_formatted = datetime.strptime(end_date, '%m-%d-%Y')
 
-    if end_date_formatted <= yesterday:
-        return True
-    else:
-        return False
+    return end_date_formatted <= yesterday
