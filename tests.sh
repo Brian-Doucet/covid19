64af7471c942eadc9f97b4edf1f4dadf101be26e
Brian <doucetba@gmail.com>
2020-06-23 08:51:17 -0400

    Removed duplicate line for cov-report term

--- tests.sh
+++ tests.sh
@@ -3,9 +3,6 @@
 # Call pytest for testing suite
 pytest -v --disable-pytest-warnings \
 
-# Show coverage report in the terminal
---cov-report term --cov=covid19 \
-
 # Show which statements are missing coverage
 --cov-report=term-missing \
 
